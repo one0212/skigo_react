f9e78fe8184dcb6afa2183240861052c94e39657
lucky4bettty <40679634+lucky4bettty@users.noreply.github.com>
2019-11-07 14:49:12 +0800

    1107- coachList 改為涵式型元件

--- src/coach/CoachList.js
+++ src/coach/CoachList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React , { useState, useEffect }  from 'react';
 import {Container ,row , Accordion , Card} from 'react-bootstrap'
 import '../css/coach_list.css';
 import $ from 'jquery'
@@ -7,111 +7,100 @@ import FilterLeftMenu from '../components/goods/FilterLeftMenu';
 import FliterRightContent from '../components/goods/FliterRightContent';
 import FilterSortWeb from '../components/goods/FilterSortWeb'
 
-
-class CoachList extends React.Component{
-    constructor(){
-      super()
-      this.state = {}
-    }
-
-    componentDidMount(){
-        // 上方排序選擇互動
-        // $('.coach-arrange a').click(function(){
-        //     $(this).css('border-bottom', '2px solid  #FD702D').siblings().css('border-bottom', '2px solid transparent');
-        // });
-        $('.coach-arrange a').click((e) => {
-            $(e.currentTarget).css('border-bottom', '2px solid  #FD702D').siblings().css('border-bottom', '2px solid transparent');
-          });
-        // rwd 上方選單*2
-            let coach_rwd_btn_state = true;
-            $('.coach-rwd-sort').hide();
-
-
-            $('#coach-btn-rwd1').click((e) => {
-            if (coach_rwd_btn_state == false) {
-                $('#coach-btn-rwd1-1').hide();
-                coach_rwd_btn_state = true;
-            } else {
-                $('#coach-btn-rwd1-1').show();
-                coach_rwd_btn_state = false;
-            }
-            console.log(coach_rwd_btn_state);
-            });
-
-            $('#coach-btn-rwd2').click((e) => {
+   
+    function CoachList() {
+        useEffect(() => {
+            $('.coach-arrange a').click((e) => {
+                $(e.currentTarget).css('border-bottom', '2px solid  #FD702D').siblings().css('border-bottom', '2px solid transparent');
+              });
+            // rwd 上方選單*2
+                let coach_rwd_btn_state = true;
+                $('.coach-rwd-sort').hide();
+    
+    
+                $('#coach-btn-rwd1').click((e) => {
                 if (coach_rwd_btn_state == false) {
-                  $('#coach-btn-rwd2-1').hide();
-                  coach_rwd_btn_state = true;
+                    $('#coach-btn-rwd1-1').hide();
+                    coach_rwd_btn_state = true;
                 } else {
-                  $('#coach-btn-rwd2-1').show();
-                  coach_rwd_btn_state = false;
+                    $('#coach-btn-rwd1-1').show();
+                    coach_rwd_btn_state = false;
+                }
+                console.log(coach_rwd_btn_state);
+                });
+    
+                $('#coach-btn-rwd2').click((e) => {
+                    if (coach_rwd_btn_state == false) {
+                      $('#coach-btn-rwd2-1').hide();
+                      coach_rwd_btn_state = true;
+                    } else {
+                      $('#coach-btn-rwd2-1').show();
+                      coach_rwd_btn_state = false;
+                    }
+                  });
+    
+            // rwd-排序選擇互動
+            // $('.rwd-drop-style').click(function(){
+            //     $(this).css('border-left', '2px solid  #FD702D').siblings().css('border-left', '2px solid transparent');
+            // });
+    
+            //左方側欄動畫
+              let move = true;
+      
+            $('#menu_active').click((e)=>{
+                if (move === true) {
+                $('#left').css({
+                    width: '0%',
+                    transform: 'translateX(-240px)',
+                });
+                $('.right_content').css('width', '100%');
+                $('.hot_img').css('height', '200px');
+                move = false;
+                } else {
+                $('#left').css({
+                    width: '25%',
+                    transform: 'translateX(0px)',
+                });
+                $('.right_content').css('width', '75%');
+                $('.hot_img').css('height', '150px');
+                move = true;
+                }
+            
+                const img_width = $('.coach-card').width();
+                console.log(img_width);
+                const img_height = 150 * 4 / 3;
+            });
+            // 左方側欄手風琴上下
+              let up_down_icon = true;
+            
+              $('.fa-angle-up').show();
+              $('.fa-angle-down').hide();
+            
+            //   $('.coach-select').click((e) => {
+            //     if (up_down_icon == true) {
+            //       e.currentTarget.find('.fa-angle-up').hide();
+            //       e.currentTarget.find('.fa-angle-down').show();
+            //       up_down_icon = false;
+            //     } else {
+            //     e.currentTarget.find('.fa-angle-up').show();
+            //     e.currentTarget.find('.fa-angle-down').hide();
+            //       up_down_icon = true;
+            //     }
+            //   });
+            $('.coach-select').click((e)=>{
+                if (up_down_icon == true) {
+                $(e.currentTarget).find('.fa-angle-up').show();
+                $(e.currentTarget).find('.fa-angle-down').hide();
+                  up_down_icon = false;
+                } else {
+                $(e.currentTarget).find('.fa-angle-up').hide();
+                $(e.currentTarget).find('.fa-angle-down').show();
+                  up_down_icon = true;
                 }
               });
-
-        // rwd-排序選擇互動
-        // $('.rwd-drop-style').click(function(){
-        //     $(this).css('border-left', '2px solid  #FD702D').siblings().css('border-left', '2px solid transparent');
-        // });
-
-        //左方側欄動畫
-          let move = true;
-  
-        $('#menu_active').click((e)=>{
-            if (move === true) {
-            $('#left').css({
-                width: '0%',
-                transform: 'translateX(-240px)',
-            });
-            $('.right_content').css('width', '100%');
-            $('.hot_img').css('height', '200px');
-            move = false;
-            } else {
-            $('#left').css({
-                width: '25%',
-                transform: 'translateX(0px)',
-            });
-            $('.right_content').css('width', '75%');
-            $('.hot_img').css('height', '150px');
-            move = true;
-            }
-        
-            const img_width = $('.coach-card').width();
-            console.log(img_width);
-            const img_height = 150 * 4 / 3;
-        });
-        // 左方側欄手風琴上下
-          let up_down_icon = true;
-        
-          $('.fa-angle-up').show();
-          $('.fa-angle-down').hide();
-        
-        //   $('.coach-select').click((e) => {
-        //     if (up_down_icon == true) {
-        //       e.currentTarget.find('.fa-angle-up').hide();
-        //       e.currentTarget.find('.fa-angle-down').show();
-        //       up_down_icon = false;
-        //     } else {
-        //     e.currentTarget.find('.fa-angle-up').show();
-        //     e.currentTarget.find('.fa-angle-down').hide();
-        //       up_down_icon = true;
-        //     }
-        //   });
-        $('.coach-select').click((e)=>{
-            if (up_down_icon == true) {
-            $(e.currentTarget).find('.fa-angle-up').show();
-            $(e.currentTarget).find('.fa-angle-down').hide();
-              up_down_icon = false;
-            } else {
-            $(e.currentTarget).find('.fa-angle-up').hide();
-            $(e.currentTarget).find('.fa-angle-down').show();
-              up_down_icon = true;
-            }
-          });
-
-    }
-  
-    render(){
-        return(
+    
+          })
+        return (
             <>
                 <div className="container">
                     {/* part1 麵包屑 */}
@@ -228,9 +217,9 @@ class CoachList extends React.Component{
    
             </>
         )
-    }
-   
-  }
+      }
+  
+
 
 
 export default CoachList;
