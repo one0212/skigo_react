aabe276eeecf26849790c9fde7859ce6ec59bb11
Allen <asd9fgh9@gmail.com>
2019-11-16 12:54:38 +0800

    card輪播修正

--- src/components/goods/Carousels.js
+++ src/components/goods/Carousels.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import '../../css/card_carousels.css';
-// import CoachCardCarosol from '../../coach/CoachCardCarosol'
-import ClassCard from '../../coach/ClassCard';
-import CardCarousels from './CardCarousels';
-import CardCarouselsCopy from './CardCarouselsCopy';
+import Slider from "react-slick";
 
 class Carousels extends React.Component{
   constructor(){
@@ -12,52 +9,104 @@ class Carousels extends React.Component{
   }
 
   render(){
+    const settings = {
+      className: "center",
+      infinite: true,
+      centerPadding: "45px",
+      slidesToShow: 3, //一次顯示4個
+      slidesToScroll: 1, //切換下一頁時移動1個
+      swipeToSlide: true,
+      responsive: [
+        {
+            breakpoint: 1300, // RWD在1024寬度時切換顯示數量
+            settings: {
+                slidesToShow: 2, //一次顯示3個
+                slidesToScroll: 1,//切換下一頁時移動3個
+           }
+       },{
+            breakpoint: 896,// RWD在600寬度時切換顯示數量
+            settings: {
+                slidesToShow: 1,//一次顯示2個
+                slidesToScroll: 1,//切換下一頁時移動2個
+           }
+       },
+      ]
+    };
      return (
       <>
-      <div>
       <section className="area_lesson">
         <div className="container">
                 <div className="lesson_title">
                     <div className="d-flex"><div className="title_sign"></div><h4>課程</h4></div>
                 </div>
                 <hr/>
-        <div id="carouselExampleControls1" className="carousel slide lesson_slide d-flex justify-content-center" data-ride="carousel">
-            <div className="carousel-inner">
-
-            <div className="carousel-item active">
-                <div className="lesson d-flex justify-content-center">
-                <CardCarousels/>
-                <CardCarousels/>
-                <CardCarousels/>
-                </div>
-            </div>
-
-            <div className="carousel-item">
-                <div className="lesson d-flex justify-content-center">
-                    {/* <!-- lesson1 --> */}
-                    <CardCarousels/>
-                    {/* <!-- lesson2 --> */}
-                    <CardCarouselsCopy/>
-                    {/* <!-- lesson3 --> */}
-                    <CardCarouselsCopy/>
-                </div>
-            </div>
-            
-                <a className="carousel-control-prev" href="#carouselExampleControls1" role="button" data-slide="prev">
-                <i className="fas fa-chevron-left icon-style"></i>
-                <span className="sr-only">Previous</span>
-                </a>
-                <a className="carousel-control-next" href="#carouselExampleControls1" role="button" data-slide="next">
-                <i className="fas fa-chevron-right icon-style"></i>
-                <span className="sr-only">Next</span>
-                </a>
-            </div>
         </div>
+        <div className="container lesson">
+            <Slider {...settings}>
+                <div>
+                    <div className="card" style={{width: "18rem"}}>
+                        <img className="home_img" src={require('../../images/coach/coach.jpg')} alt=""/>
+                        <div className="card-body">
+                            <h5 className="card-title">富良野滑雪場</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className="card" style={{width: "18rem"}}>
+                        <img className="home_img" src={require('../../images/coach/coach.jpg')} alt=""/>
+                        <div className="card-body">
+                            <h5 className="card-title">富良野滑雪場</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className="card" style={{width: "18rem"}}>
+                        <img className="home_img" src={require('../../images/coach/coach.jpg')} alt=""/>
+                        <div className="card-body">
+                            <h5 className="card-title">富良野滑雪場</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className="card" style={{width: "18rem"}}>
+                        <img className="home_img" src={require('../../images/coach/coach.jpg')} alt=""/>
+                        <div className="card-body">
+                            <h5 className="card-title">富良野滑雪場</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className="card" style={{width: "18rem"}}>
+                        <img className="home_img" src={require('../../images/coach/coach.jpg')} alt=""/>
+                        <div className="card-body">
+                            <h5 className="card-title">富良野滑雪場</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className="card" style={{width: "18rem"}}>
+                        <img className="home_img" src={require('../../images/coach/coach.jpg')} alt=""/>
+                        <div className="card-body">
+                            <h5 className="card-title">富良野滑雪場</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            </Slider>
         </div>
-        </section>
-        
-      </div>
-    </>
+      </section>
+      </>
     )
   }
  
