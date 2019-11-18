6dc3eff5773ca27b04b5775644c9b6e8e272515f
lucky4bettty <40679634+lucky4bettty@users.noreply.github.com>
2019-11-18 19:01:15 +0800

    20191118-路由設定初步完成
    20191118-路由設定初步完成

--- src/coach/CoachCarousels2.js
+++ src/coach/CoachCarousels2.js
@@ -1,4 +1,6 @@
 import React from 'react'
+// import '../css/card_carousels.css'
+import Slider from 'react-slick'
 
 
 class CoachCarousels2 extends React.Component {
@@ -36,7 +38,7 @@ class CoachCarousels2 extends React.Component {
     } finally {
       await setTimeout(() => this.setState({ loading: false }))
       // await this.setState({ loading: false })
-// console.log('看我')
+      // console.log('看我')
       console.log(this.state.total.length)
     }
   }
@@ -45,12 +47,33 @@ class CoachCarousels2 extends React.Component {
   //   }}
 
   render() {
-    
-    
+    const settings = {
+      className: 'center',
+      infinite: true,
+      centerPadding: '45px',
+      slidesToShow: 3, //一次顯示4個
+      slidesToScroll: 1, //切換下一頁時移動1個
+      swipeToSlide: true,
+      responsive: [
+        {
+          breakpoint: 1300, // RWD在1024寬度時切換顯示數量
+          settings: {
+            slidesToShow: 2, //一次顯示3個
+            slidesToScroll: 1, //切換下一頁時移動3個
+          },
+        },
+        {
+          breakpoint: 896, // RWD在600寬度時切換顯示數量
+          settings: {
+            slidesToShow: 1, //一次顯示2個
+            slidesToScroll: 1, //切換下一頁時移動2個
+          },
+        },
+      ],
+    }
     return (
       <>
         <>
-
           {console.log(this.props.filter)}
           {/* { if( this.props.filter == true ){
 
@@ -63,110 +86,50 @@ class CoachCarousels2 extends React.Component {
             </div>
           ) : (
             <>
-              <div className="">
-                <section className="area_lesson">
-                  <div className="container">
-                    <div className="lesson_title">
-                      <div className="d-flex">
-                        <div className="title_sign"></div>
-                        <h4>教練</h4>
-                      </div>
-                    </div>
-                    <hr />
-                    <div
-                      id="carouselExampleControls2"
-                      className="carousel slide lesson_slide d-flex justify-content-center"
-                      data-ride="carousel"
-                    >
-                      <div className="carousel-inner">
-                        <div className="carousel-item active">
-                          <div className="lesson d-flex justify-content-center">
-                          
-                          {/* this.state.total.length
-                            {this.state.map((value, index) => {
-                              if(){
-
-                              }
-                              
-                            })} */}
-
-                            
-
-                            {this.state.total.map((value, index) => {
-                              if (!value) return <></>
-                              console.log(value.class_sid)
-                              {/* if(this.state.total.length%3 == 1){
-                                return (<div className="carousel-item active">
-                                  <div className="lesson d-flex justify-content-center">)
-                              } */}
-                              return (
-                                <div className="coach ">
-                                  <a href="#">
-                                    <div className="coach_img">
-                                      <img
-                                        className="coach_img_pic"
-                                        src={'/images/coach/' + value.coach_pic}
-                                        alt=""
-                                      />
-                                    </div>
-                                    <div>
-                                      <h3>{value.coach_name}</h3>
-                                      <p className="coach_introduce">
-                                        {value.coach_intr}
-                                      </p>
-                                      <button className="btn">預約教練</button>
-                                    </div>
-                                  </a>
-                                </div>
-                              )
-
-                              {/* if(this.state.total.length%3 == 0){
-                                return </div></div>
-                              } */}
-                            })}
-                          </div>
-                        </div>
-                        <a
-                          className="carousel-control-prev"
-                          href="#carouselExampleControls2"
-                          role="button"
-                          data-slide="prev"
-                        >
-                          <i className="fas fa-chevron-left icon-style"></i>
-                          <span className="sr-only">Previous</span>
-                        </a>
-                        <a
-                          className="carousel-control-next"
-                          href="#carouselExampleControls2"
-                          role="button"
-                          data-slide="next"
-                        >
-                          <i className="fas fa-chevron-right icon-style"></i>
-                          <span className="sr-only">Next</span>
-                        </a>
-                      </div>
+              <section className="area_lesson">
+                <div className="container">
+                  <div className="lesson_title">
+                    <div className="d-flex">
+                      <div className="title_sign"></div>
+                      <h4>教練</h4>
                     </div>
                   </div>
-                </section>
-              </div>
+                  <hr />
+                </div>
+                <div className="container lesson">
+                  <Slider {...settings}>
+                    {this.state.total.map((value, index) => {
+                      if (!value) return <></>
+                      console.log(value.class_sid)
+                      return (
+                        <div>
+                          <div className="card" style={{ width: '18rem' }}>
+                            <img
+                              className="home_img"
+                              src="./images/coach/coach.jpg"
+                              alt=""
+                            />
+                            <div className="card-body">
+                              <h5 className="card-title">富良野滑雪場1</h5>
+                              <p className="card-text">
+                                Some quick example text to build on the card
+                                title and make up the bulk of the card's
+                                content.
+                              </p>
+                              <a href="#" className="btn btn-primary">
+                                Go somewhere
+                              </a>
+                            </div>
+                          </div>
+                        </div>
+                      )
+                    })}
+                  </Slider>
+                </div>
+              </section>
             </>
           )}
         </>
-
-        {/* <div className="coach ">
-        <a href="#">
-            <div className="coach_img">
-                <img className="coach_img_pic" src="./images/coach/coach.jpg" alt="" />
-            </div>
-            <div>
-                <h3>叉叉圈教練</h3>
-                  <p className="coach_introduce">
-                      專業講師開班，2小時的雙板滑雪體驗課程，教您最基礎的滑雪知識!
-                  </p>
-            <button className="btn">預約教練</button>
-            </div>
-        </a>
-        </div> */}
       </>
     )
   }
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,50 @@
-import React from 'react'
+import React  from 'react';
 
-const Contact = () => {
-    return (
+
+
+
+class Contact extends React.Component {
+    constructor() {
+      super()
+      this.state = {
+        people : [
+            {
+              name: 'Casper',
+              like: '鍋燒意麵',
+              age: 18
+            },
+            {
+              name: 'Wang',
+              like: '炒麵',
+              age: 24
+            },
+            {
+              name: 'Bobo',
+              like: '蘿蔔泥',
+              age: 1
+            },
+            {
+              name: '滷蛋',
+              like: '蘿蔔泥',
+              age: 3
+            }
+          ]
+          
+      }
+    }
+  
+   
+
+    render() {
+      return (       
         <>
-        <h1>聯絡</h1>
+        {this.state.people.filter((item, index, array)=>{
+            return item.age > 5
+        })}
+         
         </>
-    )
-}
-
-export default Contact
+      )
+    }
+  }
+  
+  export default Contact
