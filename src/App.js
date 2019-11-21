950e43e14a2efd36580a113d5224f28f7e8fa2b7
one0212 <feliciajiang12@gmail.com>
2019-11-22 02:49:23 +0800

    購物車api範例 - v1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 // import { Container } from 'react-bootstrap'
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.css'
+import Example from './components/Example'
 
 import Header from './components/navbar/Header'
 import home from './components/home/Home'
@@ -56,6 +57,8 @@ function App() {
             <Route path="/activeUser" component={AccountActiveResult} />
 
             <Route path="/product" component={Product} />
+
+            <Route path="/example" component={Example} />
           </Switch>
         </>
       </Router>
---
+++ src/components/Example.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+const Example = () => {
+  const handleClick = () => {
+    const url = 'http://localhost:3001/api/cart/items'
+    const id = document.querySelector('#product-id').textContent
+    const select = document.querySelector('#product-select')
+    const selectedValue = select.options[select.selectedIndex].value
+    const obj = {
+      // prodId, prodType, qty 這三個變數不可改
+      prodId: id, //這邊放資料庫的商品id
+      prodType: 'products', //這邊放資料庫的商品類型 例如飯店傳hotel,
+      qty: selectedValue, //商品數量 不需用字串
+    }
+    console.log(id, selectedValue, obj)
+    fetch(url, {
+      body: JSON.stringify(obj),
+      header: {
+        'content-type': 'application/json',
+      },
+      method: 'POST',
+    }).then(response => {
+      // 如果response.status拿到200表示加入商品成功
+      if (response.status === 200) {
+        // 所以可以用此判斷, 後續要做的事情就是放這邊
+        console.log('商品加入成功')
+      }
+    })
+  }
+  return (
+    <>
+      <div>雪具1</div>
+      {/* 商品id會藏起來  -- 但也許有更好的做法 */}
+      <div id="product-id" style={{ display: 'none' }}>
+        123456
+      </div>
+      <select id="product-select">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+      </select>
+      <button onClick={handleClick}>area</button>
+      {/* 此按鈕為加入購物車 */}
+    </>
+  )
+}
+
+export default Example
