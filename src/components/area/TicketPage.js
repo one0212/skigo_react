f5dc133f1181bd066d76ab916acef87d3af765d0
Allen <asd9fgh9@gmail.com>
2019-11-25 22:02:24 +0800

    購物車測試OK

--- src/components/area/TicketPage.js
+++ src/components/area/TicketPage.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import '../../css/ticket_page.css'
-import $ from 'jquery'
 import { Form } from 'react-bootstrap'
 import FilterBread2 from '../goods/FilterBread2'
 
@@ -14,6 +13,32 @@ class TicketPage extends React.Component {
     }
   }
 
+  handleClick = () => {
+    const url = 'http://localhost:3001/api/cart/items'
+    const id = document.querySelector('#ticket-id').textContent
+    const select = document.querySelector('#ticket-select')
+    const selectedValue = select.options[select.selectedIndex].value
+    const obj = {
+      // prodId, prodType, qty 這三個變數不可改
+      prodId: parseInt(id), //這邊放資料庫的商品id
+      prodType: 'areas', //這邊放資料庫的商品類型 例如飯店傳hotel,
+      qty: parseInt(selectedValue), //商品數量 不需用字串
+    }
+    console.log(id, selectedValue, obj)
+    fetch(url, {
+      body: JSON.stringify(obj),
+      headers: {
+        'content-type': 'application/json',
+      },
+      method: 'POST',
+    }).then(response => {
+      // 如果response.status拿到200表示加入商品成功
+      if (response.status === 200) {
+        // 所以可以用此判斷, 後續要做的事情就是放這邊
+        console.log('商品加入成功')
+      }
+    })
+  }
   async componentWillMount() {
     try {
       await this.setState({ loading: true })
@@ -60,6 +85,8 @@ class TicketPage extends React.Component {
       }
       return num
     }
+
+    
   }
 
   render() {
@@ -74,6 +101,9 @@ class TicketPage extends React.Component {
           <div class="ticket_book_out container">
             {/* 麵包屑 */}
             <FilterBread2 />
+            <div id="ticket-id" style={{ display: 'none' }}>
+              123456
+            </div>
             {/* 圖+內容 */}
             <div class="ticket_head row mb-5">
               {/* 圖 */}
@@ -102,10 +132,10 @@ class TicketPage extends React.Component {
                       </div>
                     </div>
                     <div className="ticket-snowfield d-flex align-items-center">
-                        <span>NT</span>
-                        <span className="ticket-price">
-                          {this.state.total[0].ticket_price}
-                        </span>
+                      <span>NT</span>
+                      <span className="ticket-price">
+                        {this.state.total[0].ticket_price}
+                      </span>
                     </div>
                     <div className="ticket-book row">
                       <div className="col-3 d-flex flex-column justify-content-around">
@@ -113,34 +143,37 @@ class TicketPage extends React.Component {
                         <h5>使用期限</h5>
                         <h5>販售期間</h5>
                       </div>
-                      
+
                       <div className="ticket-page-style col-9">
                         {/* <input id="people" value={this.state.people} onChange={this.changepeople}
                                     name="people" className="ticket_book_num" type="number" /> */}
-                          <Form.Group controlId="exampleForm.ControlSelect1">
-                            <Form.Control as="select">
-                              <option>1</option>
-                              <option>2</option>
-                              <option>3</option>
-                              <option>4</option>
-                              <option>5</option>
-                              <option>6</option>
-                              <option>7</option>
-                              <option>8</option>
-                              <option>9</option>
-                              <option>10</option>
-                            </Form.Control>
-                          </Form.Group>
-                          <p className="ticket_time_line">使用期限至{this.state.total[0].area_end_date}為止</p>
-                          <p className="ticket_date_line">
+                        <Form.Group controlId="exampleForm.ControlSelect1">
+                          <Form.Control as="select" id="ticket-select">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                            <option>6</option>
+                            <option>7</option>
+                            <option>8</option>
+                            <option>9</option>
+                            <option>10</option>
+                          </Form.Control>
+                        </Form.Group>
+                        <p className="ticket_time_line">
+                          使用期限至{this.state.total[0].area_end_date}為止
+                        </p>
+                        <p className="ticket_date_line">
                           {this.state.total[0].ticket_start_date}~
                           {this.state.total[0].ticket_end_date}
                         </p>
                       </div>
                     </div>
                     <div className="ticket-date"></div>
-                     <div>
-                      <button className="btn">
+                    <div>
+                      {/* 此按鈕為加入購物車 */}
+                      <button className="btn" onClick={this.handleClick}>
                         <i className="fas fa-shopping-cart"></i> 放入購物車
                       </button>
                       <button className="btn">
