cda332fb53cd13cbd9f8380a68943061c68b5363
bo-han-kao <54506408+bo-han-kao@users.noreply.github.com>
2019-11-26 22:11:36 +0800

    第一次
    第一次合併

--- src/components/AttractionComp/AttractionsIndex.js
+++ src/components/AttractionComp/AttractionsIndex.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 // 掛載header元件
-import Header from '../components/Header';
+// import Header from '../components/Header';
 // 掛載景點css
-import '../css/AttractionsIndex_css.css';
-
+import '../../css/AttractionsIndex_css.css';
 
 
 
@@ -11,16 +10,16 @@ class AttractionsIndex extends Component {
   render() {
     return (
       <>
-      <Header/>
+      {/* <Header/> */}
       <div className="ABoxImgPosition">
-            <img src={require('../images/AttractionsImg/earth3-right.png')} alt="" className="ABoxImgPositionImg"/>
-            <div className="AGlobal AGlobalIconPositionFu"><img src={require('../images/AttractionsImg/AmapMarker.svg')} alt="" /></div>
-            <div className="AGlobal AGlobalIconPositionJ"><img src={require('../images/AttractionsImg/AmapMarker.svg')} alt="" /></div>
-            <div className="AGlobal AGlobalIconPositionSum"><img src={require('../images/AttractionsImg/AmapMarker.svg')} alt="" /></div>
-            <div className="AGlobal AGlobalIconPositionMia"><img src={require('../images/AttractionsImg/AmapMarker.svg')} alt="" /></div>
-            <div className="AGlobal AGlobalIconPositionMia2"><img src={require('../images/AttractionsImg/AmapMarker.svg')} alt="" /></div>
-            <div className="AGlobal AGlobalIconPositionZun"><img src={require('../images/AttractionsImg/AmapMarker.svg')} alt="" /></div>
-            <div className="AGlobal AGlobalIconPositionBa"><img src={require('../images/AttractionsImg/AmapMarker.svg')} alt="" /></div>
+            <img src={require('../../images/AttractionsImg/earth3-right.png')} alt="" className="ABoxImgPositionImg"/>
+            {/* <div className="AGlobal AGlobalIconPositionFu"><img src={require('../images/AttractionsImg/AmapMarker.svg')} alt="" /></div> */}
+            <div className="AGlobal AGlobalIconPositionJ"><img src={require('../../images/AttractionsImg/AmapMarker.svg')} alt="" /></div>
+            <div className="AGlobal AGlobalIconPositionSum"><img src={require('../../images/AttractionsImg/AmapMarker.svg')} alt="" /></div>
+            {/* <div className="AGlobal AGlobalIconPositionMia"><img src={require('../images/AttractionsImg/AmapMarker.svg')} alt="" /></div> */}
+            {/* <div className="AGlobal AGlobalIconPositionMia2"><img src={require('../images/AttractionsImg/AmapMarker.svg')} alt="" /></div> */}
+            <div className="AGlobal AGlobalIconPositionZun"><img src={require('../../images/AttractionsImg/AmapMarker.svg')} alt="" /></div>
+            {/* <div className="AGlobal AGlobalIconPositionBa"><img src={require('../images/AttractionsImg/AmapMarker.svg')} alt="" /></div> */}
         </div>
 
         <div className="Acloud"> </div>
