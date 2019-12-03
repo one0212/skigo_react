7c88ba97172531c2726068a707ca4f5c21844ab3
bo-han-kao <54506408+bo-han-kao@users.noreply.github.com>
2019-12-03 11:52:23 +0800

    讓內嵌入式網頁沒右邊條
    1

--- src/components/AttractionComp/AttractionsIndex.js
+++ src/components/AttractionComp/AttractionsIndex.js
@@ -9,20 +9,21 @@ import { Carousel } from 'react-responsive-carousel';
 import Iframe from 'react-iframe'
 
 class AttractionsIndex extends Component {
-  
+
   render() {
     return (
       <>
-      <div style={{borderWidth: 0}}>
-       <Iframe url="http://localhost:3000/attractions/"
-            position="absolute"
-            width="100%"
-            id="myId"
-            className="myClassname"
-            height="100%"
-            styles={{ borderWidth: '0' }}
-            />
-        </div>
+            <div style={{ borderWidth: 0 }}>
+              <Iframe url="http://localhost:3000/attractions/"
+                position="absolute"
+                width="100%"
+                id="myId"
+                className="myClassname"
+                height="100%"
+                styles={{ borderWidth: '0' }}
+                scrolling="no"
+              />
+            </div>
       </>
     );
   }
