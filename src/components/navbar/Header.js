1fbc7d0e49e01d31def859201942850eea5e8918
one0212 <feliciajiang12@gmail.com>
2019-12-03 13:44:14 +0800

    修改header 新增捲軸事件

--- src/components/navbar/Header.js
+++ src/components/navbar/Header.js
@@ -18,6 +18,7 @@ let iconStyle = {
 }
 
 const cookies = new Cookies()
+let prevScrollPos = 0
 class Header extends Component {
   constructor(props) {
     super(props)
@@ -76,13 +77,34 @@ class Header extends Component {
     }
   }
 
+  handleScroll = () => {
+    // let prevScrollPos = window.pageYOffset
+    let currentScrollPos = window.pageYOffset
+    console.log(currentScrollPos, prevScrollPos)
+    prevScrollPos > currentScrollPos
+      ? this.setState({
+          top: '0',
+        })
+      : this.setState({
+          top: '-100px',
+        })
+
+    prevScrollPos = currentScrollPos
+    console.log(prevScrollPos)
+  }
+
   componentWillMount() {
+    window.removeEventListener('scroll', this.handleScroll)
     if (!cookies.get('role') || cookies.get('role') === 'VISITOR') {
       this.setState({ showLoginIcon: false })
     } else {
       this.setState({ showLoginIcon: true })
       this.syncCartItemQty()
     }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll)
   }
 
   signOut = () => {
@@ -122,179 +144,187 @@ class Header extends Component {
         {window.location.pathname !== '/cart' &&
         window.location.pathname !== '/checkout' &&
         window.location.pathname !== '/activeUser' ? (
-          <Navbar
-            collapseOnSelect
-            className="navbar-light pt-2 mb-2"
-            expand="lg"
-          >
-            <Container>
-              <Navbar.Brand href="/" className="ml-4">
-                <img
-                  className="logo mr-3"
-                  src="/images/small-logo.svg"
-                  alt="logo"
-                />
-              </Navbar.Brand>
-              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-              <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="mr-auto">
-                  <Nav.Link href="/ticketlist" className="nav-item mx-3">
-                    雪場票券
-                  </Nav.Link>
-                  <Nav.Link href="/CoachList" className="nav-item mx-3">
-                    教練課程
-                  </Nav.Link>
-                  <Nav.Link href="/HotelHome" className="nav-item mx-3">
-                    找飯店
-                  </Nav.Link>
-                  <Nav.Link href="/product" className="nav-item mx-3">
-                    雪具裝備
-                  </Nav.Link>
-                  <Nav.Link href="/AttractionsIndex" className="nav-item mx-3">
-                    景點排程
-                  </Nav.Link>
-                </Nav>
-                <Nav className="ml-auto">
-                  <Nav.Link>
-                    {!this.state.showLoginIcon ? (
-                      <input
-                        className="nav-item navbar-btn btn btn-outline-secondary btn-sm"
-                        type="button"
-                        value="登入｜註冊"
-                        onClick={() => this.setState({ isOpen: true })}
-                      />
-                    ) : (
-                      ''
-                    )}
-                    {this.state.showLoginIcon ? (
-                      <span>
-                        <FaRegUserCircle
-                          style={iconStyle}
-                          onClick={() =>
-                            this.setState({
-                              userDropdown: !this.state.userDropdown,
-                            })
-                          }
+          <>
+            <Navbar
+              collapseOnSelect
+              className="navbar-light pt-2 mb-2 navbar-hidden"
+              expand="lg"
+              onScroll={this.handleScroll}
+              style={{ top: this.state.top }}
+            >
+              <Container>
+                <Navbar.Brand href="/" className="ml-4">
+                  <img
+                    className="logo mr-3"
+                    src="/images/small-logo.svg"
+                    alt="logo"
+                  />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                  <Nav className="mr-auto">
+                    <Nav.Link href="/ticketlist" className="nav-item mx-3">
+                      雪場票券
+                    </Nav.Link>
+                    <Nav.Link href="/CoachList" className="nav-item mx-3">
+                      教練課程
+                    </Nav.Link>
+                    <Nav.Link href="/HotelHome" className="nav-item mx-3">
+                      找飯店
+                    </Nav.Link>
+                    <Nav.Link href="/product" className="nav-item mx-3">
+                      雪具裝備
+                    </Nav.Link>
+                    <Nav.Link
+                      href="/AttractionsIndex"
+                      className="nav-item mx-3"
+                    >
+                      景點排程
+                    </Nav.Link>
+                  </Nav>
+                  <Nav className="ml-auto">
+                    <Nav.Link>
+                      {!this.state.showLoginIcon ? (
+                        <input
+                          className="nav-item navbar-btn btn btn-outline-secondary btn-sm"
+                          type="button"
+                          value="登入｜註冊"
+                          onClick={() => this.setState({ isOpen: true })}
                         />
-                      </span>
-                    ) : (
-                      ''
-                    )}
+                      ) : (
+                        ''
+                      )}
+                      {this.state.showLoginIcon ? (
+                        <span>
+                          <FaRegUserCircle
+                            style={iconStyle}
+                            onClick={() =>
+                              this.setState({
+                                userDropdown: !this.state.userDropdown,
+                              })
+                            }
+                          />
+                        </span>
+                      ) : (
+                        ''
+                      )}
 
-                    {this.state.showLoginIcon ? (
-                      <span>
-                        <FiMail style={iconStyle} />
+                      {this.state.showLoginIcon ? (
+                        <span>
+                          <FiMail style={iconStyle} />
+                        </span>
+                      ) : (
+                        ''
+                      )}
+                      {this.state.showLoginIcon ? (
+                        <span>
+                          <AiOutlineHeart style={iconStyle} />
+                        </span>
+                      ) : (
+                        ''
+                      )}
+                      <span className="position-relative">
+                        <FiShoppingCart
+                          style={iconStyle}
+                          className="m-3"
+                          onClick={this.cartVerify}
+                        />
+                        <span
+                          className="red-point position-absolute text-center"
+                          style={{
+                            opacity: this.state.cartItemQty !== 0 ? '1' : '0',
+                          }}
+                        >
+                          {this.state.cartItemQty}
+                        </span>
                       </span>
-                    ) : (
-                      ''
-                    )}
-                    {this.state.showLoginIcon ? (
-                      <span>
-                        <AiOutlineHeart style={iconStyle} />
-                      </span>
-                    ) : (
-                      ''
-                    )}
-                    <span className="position-relative">
-                      <FiShoppingCart
-                        style={iconStyle}
-                        className="m-3"
-                        onClick={this.cartVerify}
-                      />
-                      <span
-                        className="red-point position-absolute text-center"
-                        style={{
-                          opacity: this.state.cartItemQty !== 0 ? '1' : '0',
-                        }}
-                      >
-                        {this.state.cartItemQty}
-                      </span>
-                    </span>
-                  </Nav.Link>
-                </Nav>
-                <Nav>
-                  <Nav.Link
-                    className="p-0 user-drop-list text-center position-absolute mt-3"
-                    style={{
-                      boxShadow: this.state.userDropdown
-                        ? '0 0 15px #ccc'
-                        : '0',
-                    }}
-                  >
-                    <ul
-                      className="m-0 list-unstyled drop-menu"
+                    </Nav.Link>
+                  </Nav>
+                  <Nav>
+                    <Nav.Link
+                      className="p-0 user-drop-list text-center position-absolute mt-3"
                       style={{
-                        opacity: this.state.userDropdown ? '1' : '0',
-                        maxHeight: this.state.userDropdown ? '11rem' : '0',
+                        boxShadow: this.state.userDropdown
+                          ? '0 0 15px #ccc'
+                          : '0',
                       }}
                     >
-                      <li className="my-3">
-                        <Link
-                          onClick={() => {
-                            this.setState({
-                              userDropdown: !this.state.userDropdown,
-                            })
-                          }}
-                          to="/user/profile"
-                          className="user-darkblue-text text-decoration-none"
-                        >
-                          我的帳戶
-                        </Link>
-                      </li>
-                      <li className="my-3">
-                        <Link
-                          onClick={() => {
-                            this.setState({
-                              userDropdown: !this.state.userDropdown,
-                            })
-                          }}
-                          to="/user/order"
-                          className="user-darkblue-text text-decoration-none"
-                        >
-                          我的訂單
-                        </Link>
-                      </li>
-                      <li className="my-3">
-                        <Link
-                          to="#"
-                          className="user-darkblue-text text-decoration-none"
-                        >
-                          紅利點數
-                        </Link>
-                      </li>
-                      <li className="my-3">
-                        {isGLogin ? (
-                          <GoogleLogout
-                            clientId="71115162347-h4vb50788t99f79o1pata6n1u164m3ms.apps.googleusercontent.com"
-                            buttonText="Logout"
-                            onLogoutSuccess={this.signOut}
-                            onFailure={err => console.log(err)}
-                            render={renderProps => (
-                              <Link
-                                to="/"
-                                onClick={renderProps.onClick}
-                                className="cursor-point user-darkblue-text text-decoration-none"
-                              >
-                                登出
-                              </Link>
-                            )}
-                          ></GoogleLogout>
-                        ) : (
+                      <ul
+                        className="m-0 list-unstyled drop-menu"
+                        style={{
+                          opacity: this.state.userDropdown ? '1' : '0',
+                          maxHeight: this.state.userDropdown ? '11rem' : '0',
+                        }}
+                      >
+                        <li className="my-3">
                           <Link
-                            to="/"
-                            onClick={this.signOut}
-                            className="cursor-point user-darkblue-text text-decoration-none"
+                            onClick={() => {
+                              this.setState({
+                                userDropdown: !this.state.userDropdown,
+                              })
+                            }}
+                            to="/user/profile"
+                            className="user-darkblue-text text-decoration-none"
                           >
-                            登出
+                            我的帳戶
                           </Link>
-                        )}
-                      </li>
-                    </ul>
-                  </Nav.Link>
-                </Nav>
-              </Navbar.Collapse>
-            </Container>
-          </Navbar>
+                        </li>
+                        <li className="my-3">
+                          <Link
+                            onClick={() => {
+                              this.setState({
+                                userDropdown: !this.state.userDropdown,
+                              })
+                            }}
+                            to="/user/order"
+                            className="user-darkblue-text text-decoration-none"
+                          >
+                            我的訂單
+                          </Link>
+                        </li>
+                        <li className="my-3">
+                          <Link
+                            to="#"
+                            className="user-darkblue-text text-decoration-none"
+                          >
+                            紅利點數
+                          </Link>
+                        </li>
+                        <li className="my-3">
+                          {isGLogin ? (
+                            <GoogleLogout
+                              clientId="71115162347-h4vb50788t99f79o1pata6n1u164m3ms.apps.googleusercontent.com"
+                              buttonText="Logout"
+                              onLogoutSuccess={this.signOut}
+                              onFailure={err => console.log(err)}
+                              render={renderProps => (
+                                <Link
+                                  to="/"
+                                  onClick={renderProps.onClick}
+                                  className="cursor-point user-darkblue-text text-decoration-none"
+                                >
+                                  登出
+                                </Link>
+                              )}
+                            ></GoogleLogout>
+                          ) : (
+                            <Link
+                              to="/"
+                              onClick={this.signOut}
+                              className="cursor-point user-darkblue-text text-decoration-none"
+                            >
+                              登出
+                            </Link>
+                          )}
+                        </li>
+                      </ul>
+                    </Nav.Link>
+                  </Nav>
+                </Navbar.Collapse>
+              </Container>
+            </Navbar>
+            <div style={{ width: '100%', height: '100px' }}></div>
+          </>
         ) : (
           ''
         )}
